5ca4e6b86041ed6c78e1bf1d5ecec1f58291f0c3
Orest1234 <66377934+Orest1234@users.noreply.github.com>
2022-06-13 16:54:11 +0300

    Add files via upload

---
+++ Object.h
@@ -0,0 +1,21 @@
+#pragma once
+class Object
+{
+private:
+    static unsigned int count;
+
+public:
+    static unsigned int Count()
+    {
+        return count;
+    }
+
+    Object()
+    {
+        count++;
+    }
+    ~Object()
+    {
+        count--;
+    }
+};
